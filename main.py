4f948c77efc3d80357a0e6fdd6f28e70eafac413
julesblau <jblaust1@villanova.edu>
2022-11-16 20:02:36 -0500

    Sample text using Twilio

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import os
+from twilio.rest import Client
 
 def cToFahrenheit(celsius):
     return (celsius * 1.8) + 32
@@ -18,11 +20,25 @@ def callWeatherStack(zipcode, accessKey):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    accessKey = '0b9ee0340f88066156ff8200cbd30ef0'
+    #Weatherstack
+    weatherstack_accessKey = '0b9ee0340f88066156ff8200cbd30ef0'
     zipcode = input("Enter your zipcode: ")
+    
+    #Twilio
+    twilio_account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    twilio_auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(twilio_account_sid, twilio_auth_token)
+
+    message = client.messages.create(
+        body='Hi there',
+        from_='+14254753954',
+        to='+12016614032'
+    )
+
+    print(message.sid) 
 
     if len(zipcode) == 5:
-        callWeatherStack(zipcode, accessKey)
+        callWeatherStack(zipcode, weatherstack_accessKey)
     else:
         print("Invalid Input - zipcode should be 5 numbers")
 
