4b7879b84850faaa42d5b94dec1ff394fb5af24d
julesblau <jblaust1@villanova.edu>
2022-11-16 20:57:31 -0500

    Formatted document

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import requests
 import os
 from twilio.rest import Client
 
+
 def cToFahrenheit(celsius):
     return (celsius * 1.8) + 32
 
+
 def callWeatherStack(zipcode, accessKey):
     params = {
-      'access_key': accessKey,
-      'query': zipcode
+        'access_key': accessKey,
+        'query': zipcode
     }
 
     weather_api_result = requests.get('http://api.weatherstack.com/current', params)
@@ -16,10 +18,11 @@ def callWeatherStack(zipcode, accessKey):
 
     return weather_api_response['location']['name'], cToFahrenheit(weather_api_response['current']['temperature'])
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    #Weatherstack
+    # Weatherstack
     # weatherstack_accessKey = '0b9ee0340f88066156ff8200cbd30ef0'
     weatherstack_access_key = os.environ['WEATHERSTACK_ACCESS_KEY']
     zipcode = input("Enter your zipcode: ")
@@ -29,8 +32,8 @@ if __name__ == '__main__':
         city, temperature = callWeatherStack(zipcode, weatherstack_access_key)
     else:
         print("Invalid Input - zipcode should be 5 numbers")
-    
-    #Set Twilio Credentials
+
+    # Set Twilio Credentials
     twilio_account_sid = os.environ['TWILIO_ACCOUNT_SID']
     twilio_auth_token = os.environ['TWILIO_AUTH_TOKEN']
     client = Client(twilio_account_sid, twilio_auth_token)
@@ -43,4 +46,4 @@ if __name__ == '__main__':
         to=phonenumber
     )
 
-    print(message.sid) #onlt here for testing, can be removed eventually
+    print(message.sid)  # onlt here for testing, can be removed eventually
