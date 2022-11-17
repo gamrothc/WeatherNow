0a22ea7e1d687e2c351d3afb5efa2311bbe6331b
julesblau <jblaust1@villanova.edu>
2022-11-16 20:53:01 -0500

    Switched weatherstack access key to use environment variable

--- main.py
+++ main.py
@@ -18,22 +18,23 @@ def callWeatherStack(zipcode, accessKey):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    
+
     #Weatherstack
-    weatherstack_accessKey = '0b9ee0340f88066156ff8200cbd30ef0'
+    # weatherstack_accessKey = '0b9ee0340f88066156ff8200cbd30ef0'
+    weatherstack_access_key = os.environ['WEATHERSTACK_ACCESS_KEY']
     zipcode = input("Enter your zipcode: ")
     phonenumber = input("Enter your phone number (include '+1'): ")
+
+    if len(zipcode) == 5:
+        city, temperature = callWeatherStack(zipcode, weatherstack_access_key)
+    else:
+        print("Invalid Input - zipcode should be 5 numbers")
     
-    #Twilio
+    #Set Twilio Credentials
     twilio_account_sid = os.environ['TWILIO_ACCOUNT_SID']
     twilio_auth_token = os.environ['TWILIO_AUTH_TOKEN']
     client = Client(twilio_account_sid, twilio_auth_token)
 
-    if len(zipcode) == 5:
-        city, temperature = callWeatherStack(zipcode, weatherstack_accessKey)
-    else:
-        print("Invalid Input - zipcode should be 5 numbers")
-    
     # Formulate and Send Weather Report
     weather_message = "Current temperature in " + str(city) + " is " + str(temperature) + " degrees Fahrenheit."
     message = client.messages.create(
@@ -42,4 +43,4 @@ if __name__ == '__main__':
         to=phonenumber
     )
 
-    print(message.sid) 
+    print(message.sid) #onlt here for testing, can be removed eventually
